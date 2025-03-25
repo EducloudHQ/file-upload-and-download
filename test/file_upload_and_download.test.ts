0fb4ecb2ca950156790e50f8fe7cd6b09823f2ca
vernyuy <fonchu.e.venyuy@gmail.com>
2025-03-25 05:57:49 +0100

    Initial commit

---
+++ test/file_upload_and_download.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FileUploadAndDownload from '../lib/file_upload_and_download-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/file_upload_and_download-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FileUploadAndDownload.FileUploadAndDownloadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
